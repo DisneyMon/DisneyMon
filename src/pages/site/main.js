b9959f8ad6ca4217b512885418498cc3fdeb697e
lana-ribeiro <lanac.arolinesribeiro@mail.com>
2023-11-03 12:20:52 -0300

    testando

--- src/pages/site/main.js
+++ src/pages/site/main.js
@@ -1,6 +1,2 @@
 import '../../pages/site/site';
 
-window.addEventListener("scroll",(e)=>{
-    document.querySelector(".background").style.backgroundSize = `${window.scrollY * 1.8 + 1600}px`
-    document.querySelector("#H1").style.opacity = `${(-window.scrollY + 300) * .004}`
-})
--- src/pages/site/site.jsx
+++ src/pages/site/site.jsx
@@ -46,7 +46,9 @@ export default function Site() {
 
             </section>
 
-            <script src="main.js"></script>
+            <script>
+                
+            </script>
         </body>
 
 
